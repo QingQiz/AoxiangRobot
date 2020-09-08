43137b885be3e8e6797df660e4d2564fa34a7ce1
QingQiz <sofeeys@outlook.com>
2020-09-08 18:11:02 +0800

    feat: my courses 指定学年的课程信息

--- src/classTable/classTable.py
+++ src/classTable/classTable.py
@@ -8,58 +8,64 @@ class ClassTable():
 
         self.aoxiang = Aoxiang(username, password)
 
-    def output(self, timeStart, timeEnd, **kwargs):
-        '''
+    def export(self, timeStart, timeEnd, **kwargs):
+        '''export classTable in ics format
         :param timeStart:
         :param timeEnd:
         :param **kwargs:
             :output str: output file
             :alarm int: alarm before event
         '''
-        if kwargs['output']:
-            import os
-            from .ics import ICS
-            from QingQiz import log
+        import os
+        from .ics import ICS
+        from QingQiz import log
 
-            classTable = self.aoxiang.classTable(timeStart, timeEnd)
+        classTable = self.aoxiang.classTable(timeStart, timeEnd)
 
-            header = ICS.header(f'{timeStart} - {timeEnd} 课表')
+        header = ICS.header(f'{timeStart} - {timeEnd} 课表')
 
-            body = []
-            for ct in classTable:
-                body.append(ICS.body(name=ct['title']
-                    , start=ct['startTime'][:8] + 'T' + ct['startTime'][8:]
-                    , end=ct['stopTime'][:8] + 'T' + ct['stopTime'][8:]
-                    , location=ct['location']
-                    , description=''
-                    , alarm=kwargs['alarm']
-                    , alarmDescription=''))
-            body = '\n'.join(body)
+        body = []
+        for ct in classTable:
+            body.append(ICS.body(name=ct['title']
+                , start=ct['startTime'][:8] + 'T' + ct['startTime'][8:]
+                , end=ct['stopTime'][:8] + 'T' + ct['stopTime'][8:]
+                , location=ct['location']
+                , description=''
+                , alarm=kwargs['alarm']
+                , alarmDescription=''))
+        body = '\n'.join(body)
 
-            footer = ICS.footer()
+        footer = ICS.footer()
 
-            with open(kwargs['output'], 'w') as f:
-                print(header, file=f)
-                print(body, file=f)
-                print(footer, file=f)
-        else:
-            from rich.console import Console
-            from rich.table import Table
+        with open(kwargs['output'], 'w') as f:
+            print(header, file=f)
+            print(body, file=f)
+            print(footer, file=f)
 
-            classTable = self.aoxiang.classTable(timeStart, timeEnd)
+    def output(self, term):
+        '''output current courses
+        :param term: term, for example: 19
+        '''
+        import functools
+        from rich.console import Console
+        from rich.table import Table
 
-            console = Console()
+        console = Console()
+        table = Table(title=f'20{term} 我的课程')
 
-            table = Table(title=f'{timeStart} - {timeEnd} 课表', width=120)
+        myCourses = self.aoxiang.myCourses(term)
+        # header:
+        #     0      1          2           3      4      5     6      7         8       9          10        11       12
+        #   序号 ┃ 课程序号 ┃ 课程代码 ┃ 课程名称 ┃ 教师 ┃ 学分 ┃ 校区 ┃ 课程安排 ┃ 起止周 ┃ 课程介绍 ┃ 教学大纲 ┃ 考试类型 ┃ 操作
+        header = list(map(lambda x: x[0], myCourses[0].items()))
+        # only pick up [3, 5, ..]
+        header = functools.reduce(lambda zero, x: zero + [header[x]], [3, 5, 6, 7, 8, 9, 11], [])
 
-            header = ['title', 'start', 'end', 'location']
+        for h in header:
+            table.add_column(h)
+        for course in myCourses:
+            table.add_row(*[course[h] for h in header])
 
-            for h in header:
-                table.add_column(h)
+        table.row_styles = ['none', 'dim']
 
-            for ev in classTable:
-                table.add_row(*[ev[h] for h in header])
-
-            table.row_styles = ['none', 'dim']
-
-            console.print(table)
+        console.print(table)
